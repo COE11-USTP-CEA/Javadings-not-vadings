ba25f19c198da3ed47a09e142102c0ccc054c6f5
Penyakamaayooo <jetclits@yahoo.com>
2017-03-06 20:48:51 +0800

    added /list route

--- src/main/java/com/sample/sparkdemo/MainNgaClass.java
+++ src/main/java/com/sample/sparkdemo/MainNgaClass.java
@@ -1,6 +1,8 @@
 package com.sample.sparkdemo;
 
 import com.sample.sparkdemo.model.Item;
+import com.sample.sparkdemo.model.Items;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.*;
@@ -14,25 +16,27 @@ public class MainNgaClass {
     public static void main(String[] args) {
         staticFiles.location("/public"); // Static files
         
+		Items items = new Items();
+		
         get("/testinglang", (req, res) -> {
-            Item i = new Item();
-            i.setName("pepsi");
-            i.setCode("p");
+            Item item = new Item();
+            item.setName("pepsi");
+            item.setCode("p");
 
-            Item.putItem(i);
+            Items.putItem(item);
             //Item.all();
 
-            i = new Item();
-            i.setName("coke");
-            i.setCode("c");
+            item = new Item();
+            item.setName("coke");
+            item.setCode("c");
 
-            Item.putItem(i);
+            Items.putItem(item);
 
             Map<String, Object> model = new HashMap<>();
 
             model.put("title", "testing lang");
-            model.put("items", Item.all());
-            model.put("founditem",Item.findItemByCode("p"));
+            model.put("items", Items.all());
+            model.put("founditem",Items.findItemByCode("p"));
            
             
             return new ModelAndView(model, "testinglang.ftl"); // located in src/test/resources/spark/template/freemarker
@@ -61,15 +65,41 @@ public class MainNgaClass {
             return new ModelAndView(model, "add.ftl");
         }, new FreeMarkerEngine());
 
-        get("/view/:code", (request, response) -> {
+        /*get("/view/:code", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("title", "Add New Items");
             model.put("founditem", Item.findItemByCode(request.params(":code")));
             return new ModelAndView(model, "view.ftl");
+        }, new FreeMarkerEngine()); *///penya
+		
+		get("/list", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("title", "List Items");
+            model.put("items", Items.all());
+            return new ModelAndView(model, "list.ftl");
         }, new FreeMarkerEngine());
+		
+		post("/add", (req,res) -> {
+                String code;
+                String name;
 
+            Map<String, Object> model = new HashMap<>();
+                code = req.queryParams("code");
+                name = req.queryParams("name");
 
+            Item item = new Item();
+            item.setCode(code);
+            item.setName(name);
 
+            Items.putItem(item);
+
+            
+            // model.put("code", code);
+            // model.put("name", name);
+            model.put("item", item);
+            return new ModelAndView(model, "show.ftl");
+        }, new FreeMarkerEngine());
+		
 
         post("/create", (req,res) -> {
                 String from_input1;
